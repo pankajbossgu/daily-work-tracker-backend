9981eef9735f23795f6a12d7eee67df3a038d8b5
Pankaj Rawat <51715572+pankajbossgu@users.noreply.github.com>
2025-10-30 01:17:33 +0530

    Update auth.js

--- middleware/auth.js
+++ middleware/auth.js
@@ -1,22 +1,26 @@
-const jwt = require('jsonwebtoken');
+// middleware/auth.js
+
+const jwt = require("jsonwebtoken");
 
 const authenticateToken = (req, res, next) => {
-  const token = req.header('Authorization')?.replace('Bearer ', '');
-  if (!token) return res.status(401).json({ message: 'Access denied, no token provided' });
+  const token = req.header("Authorization")?.replace("Bearer ", "");
+  if (!token) {
+    return res.status(401).json({ message: "Access denied, no token provided" });
+  }
 
   try {
     const decoded = jwt.verify(token, process.env.JWT_SECRET);
     req.user = decoded;
     next();
   } catch (error) {
-    res.status(400).json({ message: 'Invalid token' });
+    res.status(400).json({ message: "Invalid token" });
   }
 };
 
 const authorizeRoles = (...roles) => {
   return (req, res, next) => {
     if (!roles.includes(req.user.role)) {
-      return res.status(403).json({ message: 'Forbidden: Insufficient rights' });
+      return res.status(403).json({ message: "Forbidden: Insufficient rights" });
     }
     next();
   };
